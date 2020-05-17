a7875af920b6ccde1f15dba65ad0310fbc9dd32d
arjungangil2103 <47173762+arjungangil2103@users.noreply.github.com>
2020-05-17 14:16:27 +0530

    LIVE TSETST12
    TEST123

--- application.py
+++ application.py
@@ -110,7 +110,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations - VIRAJETECH</h1>
+    <h1>Congratulations - VIRAJETECH LIVE</h1>
     <p>Your first VIRAJETECH - AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
   </div>
   
